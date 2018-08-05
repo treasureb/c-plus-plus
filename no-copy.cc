ff72c28649e4ec91d9289befd676542f8123987f
treasureb <929856540@qq.com>
2018-08-05 17:11:48 +0800

    拒绝对象拷贝,该如何实现

---
+++ no-copy.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+
+/*
+ *
+ * 1. 不想被拷贝的对象,应该将其拷贝构造和构造函数声明为private
+ * 2. C++11中可采用delete关键字,指明其不能被拷贝,default关键字表示为缺省可拷贝
+ *
+ */
+
+class NoCopy{
+public:
+    NoCopy();
+private:
+    NoCopy(NoCopy& copy);
+    NoCopy& operator=(const NoCopy& copy);
+    int val_;
+};
+
+int main(){
+    NoCopy n;
+    NoCopy c;
+    n = c;
+    return 0;
+}
